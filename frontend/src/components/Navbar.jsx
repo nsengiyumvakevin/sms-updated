41b70ae0b39aeeb98b8608815ef9edb424291b0d
MDesign-Tech <mdesignpro10@gmail.com>
2025-08-31 22:59:12 +0200

    Navbar background color chande to yellow

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-
-export default function Navbar(){
+export default function Navbar() {
   const navigate = useNavigate();
-  const user = JSON.parse(localStorage.getItem('user') || 'null');
-  function logout(){
-    localStorage.removeItem('token');
-    localStorage.removeItem('user');
-    navigate('/login');
+  const user = JSON.parse(localStorage.getItem("user") || "null");
+  function logout() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    navigate("/login");
   }
   return (
-    <nav className="p-4 bg-green-500 flex justify-between">
+    <nav className="flex justify-between p-4 bg-yellow-500">
       <div className="font-bold">School Management System</div>
       <div className="space-x-4 text-2xl">
-        {!user && <Link to="/login" >Login</Link>}
+        {!user && <Link to="/login">Login</Link>}
         {!user && <Link to="/signup">Signup</Link>}
-        {user && <span>{user.name} ({user.role})</span>}
-        {user && <button onClick={logout} className="ml-2 text-red-600">Logout</button>}
+        {user && (
+          <span>
+            {user.name} ({user.role})
+          </span>
+        )}
+        {user && (
+          <button onClick={logout} className="ml-2 text-red-600">
+            Logout
+          </button>
+        )}
       </div>
     </nav>
-  )
+  );
 }
